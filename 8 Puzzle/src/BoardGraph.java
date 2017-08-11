cd1ef9bb42ab738dcc5d77cf75b80930bbe81ecd
Michael Cruz <michaelmcruz@cpp.edu>
2017-08-11 03:04:10 -0700

    Created Command Line Interface, Introduced Random Puzzle Generation & Manual Generation

--- 8 Puzzle/src/BoardGraph.java
+++ 8 Puzzle/src/BoardGraph.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -16,83 +16,19 @@ public class BoardGraph {
 		root = new BoardNode(board);
 	}
 	
-	public List<BoardNode> aStarH1() {
+	public List<BoardNode> aStar(Comparator<BoardNode> comparator) {
 		PriorityQueue<BoardNode> frontier;
 		Hashtable<BoardNode, Boolean> explored;
-		frontier = new PriorityQueue<BoardNode>(10, new H1Comparator());
+		frontier = new PriorityQueue<BoardNode>(10, comparator);
 		frontier.add(root);
 		explored = new Hashtable<BoardNode, Boolean>();
 		
 		BoardNode node = frontier.peek();
-		
 		if (!node.isSolvable()) {
 			return null;
 		}
 		
-		while (true) {
-			if (frontier.isEmpty()) {
-				return null;
-			}
-			
-			node = frontier.poll();
-			
-			if (node.isGoal()) {
-				List<BoardNode> returnList = new ArrayList<BoardNode>();
-				while(node.getParent() != null) {
-					returnList.add(0, node);
-					node = node.getParent();
-				}
-				returnList.add(0, node);
-				return returnList;
-			}
-			
-			explored.put(node, true);
-			
-			if (node.getDepth() <= 31) { // All 8 Puzzle problems can be solved in 31 moves or less.
-			
-				List<Coordinates> actions = node.getState().getAdjacent();
-				BoardNode actionNode;
-			
-				for (Coordinates coords : actions) {
-					actionNode = node.childNode(coords);
-					if (!frontier.contains(actionNode) && !explored.containsKey(actionNode)) {
-						frontier.add(actionNode);
-					} else if (frontier.contains(actionNode)) {
-						Iterator<BoardNode> it = frontier.iterator();
-						BoardNode itNode;
-						while (it.hasNext()) {
-							itNode = it.next();
-							if (itNode.equals(actionNode) && itNode.getPathCost() + itNode.h1() > actionNode.getPathCost() + actionNode.h1()) {
-								frontier.remove(actionNode);
-								frontier.add(actionNode);
-								break;
-							}
-						}
-					}
-				}
-			}
-			
-		}
-	}
-	
-	public List<BoardNode> aStarH2() {
-		PriorityQueue<BoardNode> frontier;
-		Hashtable<BoardNode, Boolean> explored;
-		frontier = new PriorityQueue<BoardNode>(10, new H2Comparator());
-		frontier.add(root);
-		explored = new Hashtable<BoardNode, Boolean>();
-		
-		BoardNode node = frontier.peek();
-		
-		if (!node.isSolvable()) {
-			return null;
-		}
-		
-		while (true) {
-			if (frontier.isEmpty()) {
-				return null;
-			}
-			System.out.println(frontier.peek() + " " + frontier.peek().getPathCost() + " " + frontier.peek().h2());
+		while (!frontier.isEmpty()) {
 			node = frontier.poll();
 			if (node.isGoal()) {
 				List<BoardNode> returnList = new ArrayList<BoardNode>();
@@ -105,22 +41,20 @@ public class BoardGraph {
 			}
 			explored.put(node, true);
 			
-			if (node.getDepth() <= 31) {
 			
-				List<Coordinates> actions = node.getState().getAdjacent();
-				BoardNode actionNode;
+			List<Coordinates> actions = node.getState().getAdjacent();
+			BoardNode actionNode;
 			
-				for (Coordinates coords : actions) {
-					actionNode = node.childNode(coords);
-					if (!frontier.contains(actionNode) && !explored.containsKey(actionNode)) {
-						frontier.add(actionNode);
-					} else if (frontier.contains(actionNode)) {
-						frontier.add(actionNode);
-						frontier.remove(actionNode);
-					}
+			for (Coordinates coords : actions) {
+				actionNode = node.childNode(coords);
+				if (!frontier.contains(actionNode) && !explored.containsKey(actionNode)) {
+					frontier.add(actionNode);
+				} else if (frontier.contains(actionNode)) {
+					frontier.add(actionNode);
+					frontier.remove(actionNode);
 				}
-			}
-			
+			}	
 		}
+		return null;
 	}
 }
--- 8 Puzzle/src/Main.java
+++ 8 Puzzle/src/Main.java
@@ -2,40 +2,134 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 	public static void main(String[] args) {
-		int[][] parsedArray = new int[3][3];
-		Scanner fileReader = null;
-		try {
-			fileReader = new Scanner(new File("input.txt"));
-		} catch (IOException e) { 
-			System.out.println("Missing input.txt!");
-			System.exit(1);
-		}
+		System.out.println("8 Puzzle Solver\n\nWritten Michael Cruz\nCPP CS420 Artifical Intelligence\nProject 1\n\n");
+		Scanner kb = new Scanner(System.in);
 		
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
-				parsedArray[i][j] = fileReader.nextInt();
+		String response = "";
+		while (true) {
+			System.out.println("\n\nOptions:\n    (1) Entr(y|ies) from input.txt\n    (2) Manual Entry\n    (3) Generate Random Puzzle\n    (4) Exit\n\nEnter Option:");
+			response = kb.nextLine();
+			int[][] parsedArray;
+			
+			switch (response) {
+			case "1":
+				parsedArray = new int[3][3];
+				Scanner fileReader = null;
+				try {
+					fileReader = new Scanner(new File("input.txt"));
+				} catch (IOException e) { 
+					System.out.println("Missing input.txt!");
+					continue;
+				}
+				while (fileReader.hasNextInt()) {
+					int token = fileReader.nextInt();
+					if (token > 8) {
+						for(int i = 2; i >= 0; i--) {
+							for(int j = 2; j >= 0; j--) {
+								parsedArray[i][j] = token % 10;
+								token /= 10;
+							}
+						}
+					} else {
+						parsedArray[0][0] = token;
+						for(int i = 0; i < 3; i++) {
+							for(int j = 0; j < 3; j++) {
+								if (fileReader.hasNextInt() && !(i == 0 && j == 0)) {
+									parsedArray[i][j] = fileReader.nextInt();
+								}
+							}
+						}
+					}
+					BoardGraph tree = new BoardGraph(new Board(parsedArray));
+					test(tree);
+				}
+				break;
+			case "2":
+				System.out.println("Enter Puzzle String (no spaces): ");
+				int token = Integer.parseInt(kb.nextLine());
+				parsedArray = new int[3][3];
+				for(int i = 2; i >= 0; i--) {
+					for(int j = 2; j >= 0; j--) {
+						parsedArray[i][j] = token % 10;
+						token /= 10;
+					}
+				}
+				BoardGraph tree = new BoardGraph(new Board(parsedArray));
+				test(tree);
+				break;
+			case "3":
+				System.out.println("Enter Depth of Randomly Generated Puzzle: ");
+				test(new BoardGraph(generatePuzzle(Integer.parseInt(kb.nextLine()))));
+				break;
+			case "4":
+				System.out.println("Okay. Goodbye.");
+				System.exit(0);
+				break;
+			default:
+				System.out.println("Invalid Option! Try again!");
+				continue;
 			}
+			
+		
 		}
+	}
+	
+	public static void test(BoardGraph tree) {
+		List<BoardNode> solution;
 		
-		BoardGraph tree = new BoardGraph(new Board(parsedArray));
-		
-		System.out.println("Beginning A*(h2) Search...");
-		
-		long startTime = System.nanoTime();
-		List<BoardNode> solution = tree.aStarH2();
-		startTime = System.nanoTime() - startTime;
-		
+		System.out.println("Beginning A*(h1) Search...");
+		long startTime1 = System.nanoTime();
+		solution = tree.aStar(new H1Comparator());
+		startTime1 = System.nanoTime() - startTime1;
 		if (solution != null) {
 			System.out.println("\nNote: Step 0 is initial state re-cap");
 			for(int i = 0; i < solution.size(); i++) {
 				System.out.println("=========[ Step " + i + " ]==========\n\nAction - " + solution.get(i).getAction() + "\n\nResult:\n" + solution.get(i) + "\n==============================\n");
 			}
-			System.out.println("Solution found in : " + startTime * 0.000000001 + "s" );
 		} else {
 			System.out.println("Board is unsolvable!");
 		}
+		
+		System.out.println("Beginning A*(h2) Search...");
+		long startTime2 = System.nanoTime();
+		solution = tree.aStar(new H2Comparator());
+		startTime2 = System.nanoTime() - startTime2;
+		if (solution != null) {
+			System.out.println("\nNote: Step 0 is initial state re-cap");
+			for(int i = 0; i < solution.size(); i++) {
+				System.out.println("=========[ Step " + i + " ]==========\n\nAction - " + solution.get(i).getAction() + "\n\nResult:\n" + solution.get(i) + "\n==============================\n");
+			}
+			System.out.println("H1 Solution found in : " + startTime1 + "ns" );
+			System.out.println("H2 Solution found in : " + startTime2 + "ns" );
+		} else {
+			System.out.println("Board is unsolvable!");
+		}
+	}
+	
+	public static Board generatePuzzle(int depth) {
+		int[][] table = new int[3][3];
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				table[i][j] = (i*3) + j;
+			}
+		}
+		Board board = new Board(table);
+		if (depth > 0) {
+			Board lastState = null;
+			Board newState = null;
+			for(int i = 0; i < depth; i++) {
+				newState = board.getPossibleStates().get(board.getAdjacent().get(ThreadLocalRandom.current().nextInt(0, board.getPossibleStates().size() - 1)).getActionIdentifier());
+				while (lastState != null && lastState.equals(newState)) {
+					newState = board.getPossibleStates().get(board.getAdjacent().get(ThreadLocalRandom.current().nextInt(0, board.getPossibleStates().size() - 1)).getActionIdentifier());
+				}
+				lastState = board;
+				board = newState;
+			}
+		}
+		return board;
 	}
 }
